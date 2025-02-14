5974e8d1c051c02817afba677cd3eba33d360ffc
MykiellDeovennPagayonan <mykielldeovenn.pagayonan-20@cpu.edu.ph>
2025-02-15 05:33:19 +0800

    [Remove] Myk's details in gallery info

--- src/lib/galleryInfo.ts
+++ src/lib/galleryInfo.ts
@@ -1,15 +1,10 @@
-const name = "Mykiell";
-const artIs: ArtIs = ["Creativity", "Beauty", "Essence", "Life"];
+const name = "yourName";
+const artIs: ArtIs = ["Something1", "Something2"]; // words to decribe what art is
 const artworks: Artwork[] = [
   {
-    imageUrl: "/images/burg1.jpg",
-    title: "Burger 1",
-    description: "Image of a burger from the internet",
-  },
-  {
-    imageUrl: "/images/burg2.jpg",
-    title: "Burger 2",
-    description: "Image of a burger from the internet",
+    imageUrl: "/images/burg1.jpg", // image path and image name in /public
+    title: "Burger 1", // title of artwork
+    description: "Image of a burger from the internet", // description of artwork
   },
 ];
 
